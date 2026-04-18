efa52871ede627866b8ea267d20d098392410999
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-04-18 20:04:37 +0300

    Update solution for pandas-inspect-shape

--- study-plans/pandas-basics/pandas-inspect-shape/pandas-inspect-shape.py
+++ study-plans/pandas-basics/pandas-inspect-shape/pandas-inspect-shape.py
@@ -6,14 +6,11 @@ def inspect_dataframe(data):
     'dtypes' (dict), 'total_values' (int)
     """
     df = pd.DataFrame(data)
-    dtypes_dict = {}
-    for col in df.columns:
-        dtypes_dict[col] = str(df[col].dtypes)
     output = {
         'rows': df.shape[0],
         'cols': df.shape[1],
         'columns': list(df.columns),
-        'dtypes': dtypes_dict,
-        'total_values': df.shape[0] * df.shape[1]
+        'dtypes': {col: str(dtype) for col, dtype in df.dtypes.items()},
+        'total_values': df.size
     }
     return output
