a0c1c41f87ff0ed988ac6d30498b4e542ea0934c
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-04-27 09:44:12 +0300

    Add solution for pandas-rename-columns

---
+++ study-plans/pandas-basics/pandas-rename-columns/pandas-rename-columns.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def rename_columns(data, rename_map):
+    """
+    Returns: dict mapping renamed column names to value lists
+    """
+    return pd.DataFrame(data)\
+             .rename(columns=rename_map)\
+             .to_dict('list')
+    
