399031559511282f9b14f0bf5d104b1d4932783c
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-04-29 09:45:25 +0300

    Update solution for pandas-change-dtypes

--- study-plans/pandas-basics/pandas-change-dtypes/pandas-change-dtypes.py
+++ study-plans/pandas-basics/pandas-change-dtypes/pandas-change-dtypes.py
@@ -5,16 +5,9 @@ def change_dtype(data, column, target_type):
     Returns: list [dtypes_before, dtypes_after] (both dicts)
     """
     df = pd.DataFrame(data)
-    dtypes_before = {}
-    dtypes_after = {}
-    
-    for col in df.columns:
-        dtypes_before[col] = str(df[col].dtype)
-
+    dtypes_before = df.dtypes.astype(str).to_dict()
     df[column] = df[column].astype(target_type)
-
-    for col in df.columns:
-        dtypes_after[col] = str(df[col].dtype)
+    dtypes_after = df.dtypes.astype(str).to_dict()
 
     return [dtypes_before, dtypes_after]
         
