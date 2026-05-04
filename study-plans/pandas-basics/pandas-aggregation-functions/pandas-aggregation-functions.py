bb90b608cf19ce957ae406bb7837e739adf3f5df
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-05-04 09:56:16 +0300

    Update solution for pandas-aggregation-functions

--- study-plans/pandas-basics/pandas-aggregation-functions/pandas-aggregation-functions.py
+++ study-plans/pandas-basics/pandas-aggregation-functions/pandas-aggregation-functions.py
@@ -4,10 +4,7 @@ def multi_agg(data, group_col, value_col, funcs):
     """
     Returns: dict mapping function name to {group: value} dict
     """
-    df = pd.DataFrame(data)\
-           .groupby(group_col)[value_col]\
-           .agg(funcs)
-    output = {}
-    for func in funcs:
-        output[func] = df[func].T.to_dict()
-    return output
+    output = pd.DataFrame(data)\
+               .groupby(group_col)[value_col]\
+               .agg(funcs)
+    return output.to_dict()
