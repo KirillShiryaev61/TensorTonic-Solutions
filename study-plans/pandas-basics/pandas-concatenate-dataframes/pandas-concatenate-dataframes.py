7a472056bd1f13f3d3f527b24351a1989220eae5
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-05-13 09:52:15 +0300

    Update solution for pandas-concatenate-dataframes

--- study-plans/pandas-basics/pandas-concatenate-dataframes/pandas-concatenate-dataframes.py
+++ study-plans/pandas-basics/pandas-concatenate-dataframes/pandas-concatenate-dataframes.py
@@ -5,7 +5,9 @@ def concat_dataframes(dfs):
     Returns: list [shape, data] where shape is [rows, cols]
     """
     concat_list = [pd.DataFrame(df) for df in dfs]
-    concat_data = pd.concat(concat_list, axis=0)
+    concat_data = pd.concat(concat_list, 
+                            axis=0,
+                            ignore_index=True)
     return [
         list(concat_data.shape),
         concat_data.to_dict('list')
