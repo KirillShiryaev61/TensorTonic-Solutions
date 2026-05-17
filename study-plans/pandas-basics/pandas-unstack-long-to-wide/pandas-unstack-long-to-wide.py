f1cbb3437a805207c7d239e51d841f4af4a9e211
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-05-17 13:16:48 +0300

    Update solution for pandas-unstack-long-to-wide

--- study-plans/pandas-basics/pandas-unstack-long-to-wide/pandas-unstack-long-to-wide.py
+++ study-plans/pandas-basics/pandas-unstack-long-to-wide/pandas-unstack-long-to-wide.py
@@ -5,7 +5,7 @@ def unstack_to_wide(data, index_col, columns_col, values_col):
     Returns: dict with index_col plus one key per unique value in columns_col
     """
     return pd.DataFrame(data)\
-             .groupby([index_col, columns_col])[values_col].sum()\
+             .set_index([index_col, columns_col])[values_col]\
              .unstack()\
              .reset_index()\
              .to_dict('list')
