c3ec8e8240bcc08349b7ed810cc6504f169a81bb
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-06-18 09:52:20 +0300

    Solve sql-running-totals

--- study-plans/sql-basics/sql-running-totals/sql-running-totals.sql
+++ study-plans/sql-basics/sql-running-totals/sql-running-totals.sql
@@ -1,8 +1,14 @@
 -- Write your SQL query here
-SELECT account, 
-       txn_date, 
-       amount, 
-       SUM(amount) OVER (PARTITION BY account ORDER BY txn_date) AS running_total
+SELECT 
+    account, 
+    txn_date, 
+    amount, 
+    SUM(amount) OVER (
+        PARTITION BY account 
+        ORDER BY txn_date, id
+    ) AS running_total
 FROM transactions
-ORDER BY account ASC,
-         txn_date ASC
+ORDER BY 
+    account ASC,
+    txn_date ASC,
+    id ASC
