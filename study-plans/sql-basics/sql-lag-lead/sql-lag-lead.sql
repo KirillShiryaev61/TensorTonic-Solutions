887321753fa1e5ae6cf3160165179a9be2434251
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-06-22 09:58:25 +0300

    Solve sql-lag-lead

--- study-plans/sql-basics/sql-lag-lead/sql-lag-lead.sql
+++ study-plans/sql-basics/sql-lag-lead/sql-lag-lead.sql
@@ -1,6 +1,6 @@
 -- Write your SQL query here
 SELECT month,
        revenue,
-       LAG(revenue, 1, 0) OVER (ORDER BY id) AS prev_revenue,
-       revenue - LAG(revenue, 1, 0) OVER (ORDER BY id) AS revenue_change
+       LAG(revenue, 1, 0) OVER (ORDER BY month, id) AS prev_revenue,
+       revenue - LAG(revenue, 1, 0) OVER (ORDER BY month, id) AS revenue_change
 FROM monthly_revenue
