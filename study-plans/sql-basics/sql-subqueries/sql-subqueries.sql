9c45b54428244188027686aa00d4e596a87edd05
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-06-30 09:57:02 +0300

    Solve sql-subqueries

--- study-plans/sql-basics/sql-subqueries/sql-subqueries.sql
+++ study-plans/sql-basics/sql-subqueries/sql-subqueries.sql
@@ -1,7 +1,7 @@
 -- Write your SQL query here
 SELECT name, 
        price,
-       price - (SELECT AVG(price) FROM products) AS vs_avg
+       ROUND(price - (SELECT AVG(price) FROM products), 2) AS vs_avg
 FROM products
 WHERE id IN (SELECT DISTINCT product_id FROM sales)
 ORDER BY vs_avg DESC, 
