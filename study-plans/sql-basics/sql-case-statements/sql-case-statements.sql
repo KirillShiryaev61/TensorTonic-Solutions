5c732887cf64c115ae8571f62bfa1b7d2150bb2b
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-07-02 09:59:18 +0300

    Solve sql-case-statements

---
+++ study-plans/sql-basics/sql-case-statements/sql-case-statements.sql
@@ -0,0 +1,22 @@
+-- Write your SQL query here
+SELECT
+    username, 
+    session_count,
+    CASE 
+        WHEN session_count >= 50 THEN 'Power'
+        WHEN session_count >= 10 THEN 'Casual'
+        ELSE 'Dormant'
+    END AS activity_level,
+    CASE 
+        WHEN platform = 'ios' OR platform = 'android' THEN 'Mobile'
+        WHEN platform = 'web' OR platform = 'desktop' THEN 'Desktop'
+        ELSE 'Other'
+    END AS platform_type
+FROM user_sessions
+ORDER BY 
+    CASE activity_level
+        WHEN 'Power' THEN 1
+        WHEN 'Casual' THEN 2
+        ELSE 3
+    END,
+    username ASC
