ea08b55ef02f5d4764c2a17250d6ad80af962b72
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-07-09 10:14:55 +0300

    Solve numpy-boolean-masking

--- study-plans/numpy-basics/numpy-boolean-masking/numpy-boolean-masking.py
+++ study-plans/numpy-basics/numpy-boolean-masking/numpy-boolean-masking.py
@@ -5,7 +5,7 @@ def row_summary(data, threshold):
     data = np.array(data, dtype=np.float64)
     cond = data > threshold
 
-    el_1 = np.where(cond, 1, 0).copy()
+    el_1 = np.where(cond, 1, 0)
     el_2 = np.where(np.any(cond, axis=1)[:, np.newaxis], data, 0)
     el_3 = np.where(np.all(cond, axis=1)[:, np.newaxis], data, 0)
     return np.array([el_1, el_2, el_3])
