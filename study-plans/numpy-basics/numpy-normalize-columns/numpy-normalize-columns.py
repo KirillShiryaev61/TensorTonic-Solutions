91933760fc927c682281a9a3ea566014c194a976
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-07-21 09:59:50 +0300

    Solve numpy-normalize-columns

--- study-plans/numpy-basics/numpy-normalize-columns/numpy-normalize-columns.py
+++ study-plans/numpy-basics/numpy-normalize-columns/numpy-normalize-columns.py
@@ -3,4 +3,4 @@ import numpy as np
 def normalize(data):
     """Returns: np.ndarray of shape (m, n), z-score normalized per column"""
     data = np.array(data, dtype=np.float64)
-    return (data - data.mean(axis=0)) / data.std(axis=0)
+    return (data - data.mean(axis=0)) / np.maximum(data.std(axis=0), 1e-7)
