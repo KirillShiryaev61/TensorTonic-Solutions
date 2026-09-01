c639ecbc69d3b3280a6d9882a49b67dfaecbc4a5
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-09-01 10:13:21 +0300

    Solve pytorch-tensor-arithmetic

---
+++ study-plans/pytorch-basics/pytorch-tensor-arithmetic/pytorch-tensor-arithmetic.py
@@ -0,0 +1,22 @@
+import torch
+
+def tensor_op(x, y, op):
+    """
+    Returns: list (result tensor converted via .tolist())
+    """
+    x = torch.tensor(x)
+    y = torch.tensor(y)
+    
+    if op == 'add':
+        return torch.add(x, y).tolist()
+    elif op == 'multiply':
+        return torch.mul(x, y).tolist()
+    elif op == 'matmul':
+        return torch.matmul(x, y).tolist()
+    elif op == 'power':
+        return torch.pow(x, y).tolist()
+    elif op == 'max':
+        return torch.max(x, y).tolist()
+    else:
+        raise ValueError('Unknown op')
+        
