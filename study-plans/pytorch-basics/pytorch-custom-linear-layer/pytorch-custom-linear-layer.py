20ddce73686269bcb63f197bbfd9d5c348ef173a
Kirill Shiryaev <uwauwa61@yandex.ru>
2026-09-14 10:32:40 +0300

    Solve pytorch-custom-linear-layer

--- study-plans/pytorch-basics/pytorch-custom-linear-layer/pytorch-custom-linear-layer.py
+++ study-plans/pytorch-basics/pytorch-custom-linear-layer/pytorch-custom-linear-layer.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 
@@ -12,11 +13,12 @@ class CustomLinear(nn.Module):
             torch.empty(out_features, in_features)
         )
         self.bias = nn.Parameter(
-            nn.init.zeros_(
                 torch.empty(out_features)
-            )
         )
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+        nn.init.uniform(self.bias, -bound, bound)
 
     def forward(self, x):
-        x = x @ self.weight.T + self.bias
-        return x
+        return x @ self.weight.T + self.bias
